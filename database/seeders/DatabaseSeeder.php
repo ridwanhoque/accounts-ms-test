3d6ada75c8395adbb974bc1562add65fdd26963c
ridwan <ridwan.abdnews@gmail.com>
2021-12-29 07:51:02 +0600

    deleting unnecessary files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,28 +16,14 @@ class DatabaseSeeder extends Seeder
         $this->call(StatusesTableSeeder::class);
         $this->call(CompaniesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(ColorsTableSeeder::class);
-        $this->call(RawMaterialsTableSeeder::class);
-        $this->call(SubRawMaterialsTableSeeder::class);
-        $this->call(FmKutchasTableSeeder::class);
-        $this->call(MachinesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(SuppliersTableSeeder::class);
-        $this->call(CustomersTableSeeder::class);
-        $this->call(BatchesTableSeeder::class);
-        $this->call(SheetSizesTableSeeder::class);
         $this->call(AccountInformationSeeder::class);
         $this->call(PaymentMethodTableSeeder::class);
-        $this->call(PartyTableSeeder::class);
-        $this->call(WastageStocksTableSeeder::class);
-        $this->call(ConfigMaterialsTableSeeder::class);
-
+        
         //accounts seeder
         $this->call(OwnerTypesTableSeeder::class);
         $this->call(ChartTypesTableSeeder::class);
         $this->call(ChartOfAccountTableSeeder::class);
         //uncomment DummyChartOfAccountsTableSeeder for more chart of accounts
         $this->call(DummyChartOfAccountsTableSeeder::class);
-        $this->call(DummyPartiesTableSeeder::class);
     }
 }
